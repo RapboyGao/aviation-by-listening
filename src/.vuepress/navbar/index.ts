b3a0f62da103421689936500546e6200f8877133
RapboyGao <atgao74201@qq.com>
2025-04-22 13:11:17 +0800

    2025.04.22 删除导航栏中的多余条目

--- src/.vuepress/navbar/index.ts
+++ src/.vuepress/navbar/index.ts
@@ -5,31 +5,6 @@ const nav = navbar([
     link: "/",
     text: "Home",
   },
-  {
-    text: "English",
-    children: ["/Notes/pepec"],
-  },
-  {
-    text: "Study",
-    children: [
-      {
-        text: "运行手册",
-        children: ["/Notes/operations-manual/OM-05.md", "/Notes/operations-manual/OM-Misc.md"],
-      },
-      {
-        text: "危险品",
-        children: ["/dangerous-goods"],
-      },
-      {
-        text: "FCTM",
-        children: ["/Notes/FCTM"],
-      },
-      {
-        text: "Fleet",
-        children: ["/Notes/Fleet/3DD"],
-      },
-    ],
-  },
 ]);
 
 export default nav;
