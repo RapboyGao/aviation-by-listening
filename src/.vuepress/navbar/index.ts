270d5ba117673f7ba3686ec55b3cd4bc5723d7d0
RapboyGao <atgao74201@qq.com>
2025-04-22 13:09:29 +0800

    2025.04.22 重构导航栏，删除多余的检查清单文件

--- src/.vuepress/navbar/index.ts
+++ src/.vuepress/navbar/index.ts
@@ -1,47 +1,35 @@
 import { navbar } from "vuepress-theme-hope";
 
 const nav = navbar([
-	{
-		link: "/",
-		text: "Home",
-	},
-	{
-		text: "English",
-		children: ["/Notes/pepec"],
-	},
-	{
-		text: "Study",
-		children: [
-			{
-				text: "运行手册",
-				children: [
-					"/Notes/operations-manual/OM-05.md",
-					"/Notes/operations-manual/OM-Misc.md",
-				],
-			},
-			{
-				text: "危险品",
-				children: ["/dangerous-goods"],
-			},
-			{
-				text: "FCTM",
-				children: ["/Notes/FCTM"],
-			},
-			{
-				text: "Fleet",
-				children: ["/Notes/Fleet/3DD"],
-			},
-		],
-	},
-	{
-		text: "Checklists",
-		children: [
-			"/Checklists/before-flight-day/",
-			"/Checklists/before-flight-going/",
-			"/Checklists/direct-preparing/",
-			"/Checklists/ground-operations",
-		],
-	},
+  {
+    link: "/",
+    text: "Home",
+  },
+  {
+    text: "English",
+    children: ["/Notes/pepec"],
+  },
+  {
+    text: "Study",
+    children: [
+      {
+        text: "运行手册",
+        children: ["/Notes/operations-manual/OM-05.md", "/Notes/operations-manual/OM-Misc.md"],
+      },
+      {
+        text: "危险品",
+        children: ["/dangerous-goods"],
+      },
+      {
+        text: "FCTM",
+        children: ["/Notes/FCTM"],
+      },
+      {
+        text: "Fleet",
+        children: ["/Notes/Fleet/3DD"],
+      },
+    ],
+  },
 ]);
 
 export default nav;
